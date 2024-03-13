5632e16f72b4844c954d22fca510275eb51b798d
Async Exception <tijmenlodders@outlook.com>
2024-03-13 16:59:51 +0100

    Expended script to install zsh with oh my zsh

--- generic-ubuntu-installation.sh
+++ generic-ubuntu-installation.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+#install by using sh -c "$(curl -fsSL https://github.com/AsyncException/generic-ubuntu-installation/blob/main/generic-ubuntu-installation.sh)"
 
+#update system
 sudo apt update
 sudo apt upgrade
+
+#install git
+sudo apt install git
+
+#installing zsh and make it the default shell
+sudo apt install zsh
+chsh -s $(which zsh)
+
+#Install Oh my zsh and download the correct config
+cd ~
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -O https://github.com/AsyncException/generic-ubuntu-installation/blob/main/.zshrc
+
+#add user and add it to sudo group
+sudo adduser async
+sudo adduser async sudo
