d92905e6c3840b5dc912a400b421f1305956e772
Async Exception <tijmenlodders@outlook.com>
2024-03-13 18:26:33 +0100

    Added custom user and install checks

--- generic-ubuntu-installation.sh
+++ generic-ubuntu-installation.sh
@@ -5,29 +5,45 @@
 sudo apt update
 sudo apt upgrade -y
 
+#install curl
+installcount=$(apt list curl | grep curl --count)
+if [ installcount -eq 0 ] then
+    sudo apt install curl -y
+fi
+
 #install git
-sudo apt install git -y
+installcount=$(apt list git | grep git --count)
+if [ installcount -eq 0 ] then
+    sudo apt install git -y
+fi
 
 clear
 
 #installing zsh and make it the default shell
-sudo apt install zsh -y
-chsh -s $(which zsh)
+installcount=$(apt list zsh | grep zsh --count)
+if [ installcount -eq 0 ] then
+    sudo apt install zsh -y
+fi
 
 clear
 
+echo "enter username"
+read USER
+
 #add user and add it to sudo group
-sudo adduser async --gecos "" -s /usr/bin/zsh
-sudo adduser async sudo
+sudo adduser $USER --gecos "" -s /usr/bin/zsh
+sudo adduser $USER sudo
 
 clear
 
 #Install Oh my zsh and download the correct config
-cd /home/async
+cd "/home/${USER}"
 export RUNZSH="no"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-cp -r ~/.oh-my-zsh /home/async/.oh-my-zsh
+cp -r ~/.oh-my-zsh "/home/${USER}/.oh-my-zsh"
 curl -O https://raw.githubusercontent.com/AsyncException/generic-ubuntu-installation/main/.zshrc
 
 #disable root
 sudo passwd -l root
+
+exit
